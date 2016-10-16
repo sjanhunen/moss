17dd385124b89a16d972b189043000ba5576002c
Stefan Janhunen <sjanhunen@gmail.com>
2016-10-16 17:09:48 -0400

    Implement AR rules for build/clean

--- ar-mold.mk
+++ ar-mold.mk
@@ -1,8 +1,19 @@
-define MOLD_LIB_DEP
+define MOLD_AR_RULES
 $(MOLD_BINDIR)$$($1_archive)$(MOLD_AREXT): $$($1_object)
+
+.PHONY: $1
+$1: $(MOLD_BINDIR)$$($1_archive)$(MOLD_AREXT)
+
+.PHONY: $1_clean_ar
+$1_clean_ar:
+	rm -f $(MOLD_BINDIR)$$($1_archive)$(MOLD_AREXT)
+
+.PHONY: $1_clean
+$1_clean: $1_clean_ar
+
 endef
 
-$(foreach t, $(MOLD_TARGETS), $(eval $(call MOLD_LIB_DEP,$t))) 
+$(foreach t, $(MOLD_TARGETS), $(eval $(call MOLD_AR_RULES,$t)))
 
 $(MOLD_BINDIR)%$(MOLD_AREXT):
 	$(call MOLD_AR, $<, $@)
