60bb316627452f2781721eb08566bafec1bfd492
Stefan Janhunen <sjanhunen@gmail.com>
2017-01-08 13:37:52 -0500

    Finish skeletons for product, spore, and Makefile

--- test/test_language_c.py
+++ test/test_language_c.py
@@ -1,8 +1,15 @@
 import unittest
 import yeast_harness
 
-class TestLanguageC(unittest.TestCase):
 
-	def testSingleCSourceFile(self):
-		sf = yeast_harness.CSourceFile('source_tree/src/aa.c')
-		sf.create()
+class TestLanguageC(unittest.TestCase):
+    def testSingleCSourceFile(self):
+
+        src_file = yeast_harness.CSourceFile('tree/src/a.c')
+        product_file = yeast_harness.ProductFile('tree/mylib.a')
+        spore_file = yeast_harness.SporeFile('tree/mylib.spore', [src_file, ],
+                                             [product_file, ])
+        mk = yeast_harness.Makefile('Makefile', [spore_file, ])
+        mk.create()
+        mk.create_spores()
+        spore_file.create_source()
--- test/yeast_harness.py
+++ test/yeast_harness.py
@@ -1,5 +1,7 @@
 import os
 import os.path
+from abc import ABCMeta
+import io
 
 # Class design sketches for yest test harness
 
@@ -8,50 +10,50 @@ import os.path
 #	- Yeast.path.object
 #	- Yeast.path.executable
 
+
 class BaseFile(object):
+    def __init__(self, name):
+        self._name = name
 
-	def __init__(self, name):
-		self.name = name
+    @property
+    def name(self):
+        return self._name
 
-	def name(self):
-		return self.name
+    def touch(self):
+        pass
 
-	def symbol(self):
-		pass
+    def exists(self):
+        pass
 
-	def touch(self):
-		pass
+    def create(self, content):
+        directory = os.path.dirname(self.name)
+        if directory != '' and not os.path.isdir(directory):
+            os.makedirs(directory)
 
-	def exists(self):
-		pass
+        fout = open(self.name, "wb")
+        fout.write(bytes(content, 'UTF-8'))
+        fout.close()
 
-	def create(self, content):
-		directory = os.path.dirname(self.name)
-		if not os.path.isdir(directory):
-			os.makedirs(directory)
+    def delete(self):
+        pass
 
-		fout = open(self.name, "wb")
-		fout.write(bytes(content, 'UTF-8'))
-		fout.close()
-
-	def delete(self):
-		pass
 
 # SourceFile(BaseFile)
 #	- object_files
 #	- products
 
+
 class CSourceFile(BaseFile):
 
-	C_FILE_TEMPLATE = """
+    C_FILE_TEMPLATE = """
 void function_%s()
 {
 }
 """
+    def create(self):
+        fn_name = os.path.basename(os.path.splitext(self.name)[0])
+        super(CSourceFile, self).create(self.C_FILE_TEMPLATE % fn_name)
 
-	def create(self):
-		fn_name = os.path.basename(os.path.splitext(self.name)[0])
-		super(CSourceFile, self).create(self.C_FILE_TEMPLATE % fn_name)
 
 # HSourceFile, CppSourceFile, AsmSourceFile, etc.
 
@@ -59,14 +61,62 @@ void function_%s()
 #	- source
 #	- products
 
-# ProductFile
-#	- source_files
-#	- object_files
-#	- spore
 
-# SporeFile
-#	- products
-#	- makefile
+class ProductFile(BaseFile):
+    def __init__(self, name):
+        super(ProductFile, self).__init__(name)
 
-# Makefile
-#	- spores
+
+# Create StaticLibProductFile, ExecutableProductFile, etc.
+
+
+class SporeFile(BaseFile):
+    def __init__(self, name, source_files, products):
+        super(SporeFile, self).__init__(name)
+        self._products = products
+        self._source_files = source_files
+
+    @property
+    def source_files(self):
+        return self._source_files
+
+    @property
+    def products(self):
+        return self._products
+
+    def create(self):
+        spore_name = os.path.basename(os.path.splitext(self.name)[0])
+
+        out = io.StringIO()
+        out.write('YEAST.SPORES += %s\n' % spore_name)
+        out.write('%s.source =' % spore_name)
+        for s in self.source_files:
+            out.write(' \\\n    %s' % s.name)
+
+        super(SporeFile, self).create(out.getvalue())
+
+    def create_source(self):
+        for source in self.source_files:
+            source.create()
+
+
+class Makefile(BaseFile):
+    def __init__(self, name, spores):
+        super(Makefile, self).__init__(name)
+        self._spores = spores
+
+    @property
+    def spores(self):
+        return self._spores
+
+    def create(self):
+        out = io.StringIO()
+        for s in self.spores:
+            out.write('include %s\n' % s.name)
+        out.write("include ../yeast.mk")
+
+        super(Makefile, self).create(out.getvalue())
+
+    def create_spores(self):
+        for spore in self.spores:
+            spore.create()
