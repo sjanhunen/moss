976eb8eceb4670b8c50cb78cbea3ccf455e49a20
Stefan Janhunen <sjanhunen@gmail.com>
2017-03-25 19:24:18 -0400

    Sketch out updated class design

--- test/test_yeast.py
+++ test/test_yeast.py
@@ -3,7 +3,37 @@ from yeast_harness import *
 
 
 class TestYeast(unittest.TestCase):
-    def test_single_c_source(self):
+
+    def test_c_source_compile(self):
+
+        # New proposal for source files:
+        # 	SourceFile(name, contents, dependencies)
+        # Language-specific SourceFile subclasses create dependencies automatically
+        #
+        # h1 = CHeaderFile('lib1/inc/header.h')
+        # c1 = CSourceFile('lib1/src/file1.c', includes=h1, checks='defined(BOB)')
+        # c2 = CSourceFile('lib2/src/file2.c')
+        #
+        # sp = SporeFile('lib1/lib1.spore', source=[c1, c2],
+        #				 defines='BOB', products='static_lib')
+        # mk = Makefile('Makefile', spores=sp)
+        #
+        # with SourceTree('my_source') as st:
+        # 	# create can be used to create any node in the tree
+        # 	st.create(mk)
+        #
+        # 	build1 = Build(st, mk, arch='host', tool='gcc')
+        # 	build2 = Build(st, mk, arch='arm', tool='gcc')
+        #
+        # 	o1 = ObjectFile(c1, build1)
+        # 	o1 = ObjectFile(c1, build2)
+        #
+        #	build1.make()
+        #	assert(o1.exists())
+        #   assert(not o2.exist())
+        #
+        #	build2.make()
+        #   assert(o2.newer_than(o1))
 
         mk = Makefile(
             spores=SporeFile(
