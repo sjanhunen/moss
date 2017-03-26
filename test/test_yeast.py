8d1229505159c1f23ca23c6f2295b896942a69a5
Stefan Janhunen <sjanhunen@gmail.com>
2017-03-25 20:09:05 -0400

    Create random directories for large source tree

--- test/test_yeast.py
+++ test/test_yeast.py
@@ -50,12 +50,13 @@ class TestYeast(unittest.TestCase):
 
     def test_large_source_tree(self):
 
-        make_filename = lambda ext: ''.join(
+        make_filename = lambda ext='': ''.join(
                 random.choice(string.ascii_lowercase) for _ in range(8)) + ext
 
-        make_sources = lambda: [CSourceFile(make_filename('.c')) for _ in range(10)]
+        make_sources = lambda path: [
+                CSourceFile(path + '/' + make_filename('.c')) for _ in range(10)]
         make_spore = lambda: SporeFile(
-                sources=make_sources(),
+                sources=make_sources(make_filename()),
                 products='static_lib',
                 name=make_filename('.spore'))
 
