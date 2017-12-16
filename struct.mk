fc74c944aa0ed75095bbe07ac55efc67cf3c6ff9
Stefan Janhunen <sjanhunen@gmail.com>
2017-12-16 10:30:31 -0500

    Converge on @ style for structure fields

--- struct.mk
+++ struct.mk
@@ -1,49 +1,49 @@
 # Spore definition is through Moss structure
 
 define SPORE/example
-	name: 	example_name
+	@name: 	example_name
 
-	source: \
+	@source: \
 		file_1 \
 		file_2 \
 		file_3
 
-	artifacts: 		lib exe
+	@artifacts: 		lib exe
 
 	# Think about this approach more to see if we can get away from
 	# this style of expansion
-	my_example: 	$($1.bob)
-	c.defines: 		$(if $($1.bob), BOB_OPTION)
+	@my_example: 	$($1.bob)
+	@c.defines: 		$(if $($1.bob), BOB_OPTION)
 endef
 
 # Options are explicitly defined for spores
 
 define SPORE/example.debug
-	help: "Enable debug mode"
-	lang.c.define: DEBUG
+	@help: "Enable debug mode"
+	@lang.c.define: DEBUG
 endef
 
 define SPORE/example.march
-	help: "Select processor architecture"
+	@help: "Select processor architecture"
 endef
 
 define SPORE/example.march.x86
-	lang.c.define: 	MARCH=intel
-	source: 				src/march/intel.c
+	@lang.c.define: 	MARCH=intel
+	@source: 				src/march/intel.c
 endef
 
 define SPORE/example.march.armv5
-	lang.c.define: 	MARCH=armv5
-	source: 				src/march/armv5.c
+	@lang.c.define: 	MARCH=armv5
+	@source: 				src/march/armv5.c
 endef
 
 # Configuration is how spores are specialized for architecture
 
 define ARCH/example.armv5
 	# Must include a comment or some content before empty options
-	bob: yes
-	option: arm-yes
-	debug: no
+	@bob: yes
+	@option: arm-yes
+	@debug: no
 endef
 
 define M.def.configs
@@ -74,31 +74,21 @@ $(info Options for example: $(call M.def.options,example))
 
 $(info bob=$(if $(armv5/example.bob),ON,OFF))
 
-
-define M.def.robust_expand1
-	$(subst :,=?,$(call $1,$2))
-endef
-
-define M.def.robust_expand2:
+define M.def.robust_expand2
 	$(subst :,=?,$(subst @,$2.,$(call $1,$2)))
 endef
 
 PREFIX.fpu = bobby
 
 define STRUCT/sloppy1
-$1.one : hello sjdklsd source.c
-$1.two : bye sdjklsd $($1.fpu)
-	$1.three:another sfjdkl
+@one : hello sjdklsd source.c
+@two : bye sdjklsd $($1.fpu)
+	@three:another sfjdkl
 	#$1.four:another sfjdkl
-	$1.five: yo $(if $($1.fpu),YES,NO)
-	$1.six: humbug
+	@five: yo $(if $($1.fpu),YES,NO)
+	@six: humbug
 endef
 
-define CONST/libfreertos
-	@base_stuff:
-endef
-
-# This approach is still just a bit more readable
 define STRUCT/sloppy2
 @one : hello sjdklsd source.c
 @two : bye sdjklsd $($1.fpu)
@@ -113,7 +103,7 @@ define STRUCT/sloppy2
 	@lang.c.define: $($0.const.memory_model.$($1.memory_model))
 endef
 
-$(info $(call M.def.robust_expand1,STRUCT/sloppy1,PREFIX))
-$(eval $(call M.def.robust_expand1,STRUCT/sloppy1,PREFIX))
+$(info $(call M.def.robust_expand2,STRUCT/sloppy1,PREFIX))
+$(eval $(call M.def.robust_expand2,STRUCT/sloppy1,PREFIX))
 $(info $(call M.def.robust_expand2,STRUCT/sloppy2,PREFIX))
 $(eval $(call M.def.robust_expand2,STRUCT/sloppy2,PREFIX))
