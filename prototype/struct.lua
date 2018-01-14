0a351948cfdb614676b278b03ffca35e52b5c19b
Stefan Janhunen <sjanhunen@gmail.com>
2018-01-13 21:12:25 -0500

    Experiment with more optional constructs

--- prototype/struct.lua
+++ prototype/struct.lua
@@ -1,5 +1,9 @@
 -- TODO: prototype the equivalent of seed.mk here in lua
 
+function options(t)
+    return function(o) return t[o] end
+end
+
 seed = {
     arch = {
         armv5 = {
@@ -22,6 +26,19 @@ seed = {
                     end
                 end
         }
+    },
+    another = {
+        special1 = {},
+        special2 = {},
+        myflag = {
+            doc = "Set this for special options",
+            -- This special options "constructor" could return an
+            -- anonymous fuction that switches based upon config.
+            defines = options{
+                yes='special1',
+                no='special2'
+            }
+        }
     }
 }
 
@@ -42,4 +59,6 @@ multiple lines
 -- Print different elements of the struct
 print(seed.arch.armv5.source);
 print(seed.arch.x86.source({}));
+print(seed.another.myflag.doc);
+print(seed.another.myflag.defines('no'));
 print(multilineRecipe);
