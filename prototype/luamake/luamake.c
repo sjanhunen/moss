2484078050810da411d1ee3230558a1c4aa33ae3
Stefan Janhunen <sjanhunen@gmail.com>
2018-03-11 18:02:00 -0400

    Add initial lua integration concept

---
+++ prototype/luamake/luamake.c
@@ -0,0 +1,22 @@
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+
+int main(int argc, char *argv[])
+{
+    lua_State *ls;
+    int status;
+
+    ls = luaL_newstate();
+
+    if(argc > 1) {
+        status = luaL_loadfile(ls, argv[1]);
+        if(status) {
+            printf("ERROR: %s\n", lua_tostring(ls, -1));
+        }
+    }
+
+    lua_close(ls);
+
+    return 0;
+}
