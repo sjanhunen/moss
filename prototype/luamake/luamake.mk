6d2d088783aa65a5341d28c96458c922d7afd134
Stefan Janhunen <sjanhunen@gmail.com>
2018-03-18 21:58:04 -0400

    Sketch out initial lua-pcall & lua-dostring

--- prototype/luamake/luamake.mk
+++ prototype/luamake/luamake.mk
@@ -1,7 +1,21 @@
 load luamake.so
 
+define bob
+
+bob = function(a)
+	return a;
+end
+
+proc = function()
+	print "Hello from Lua!";
+end
+
+endef
+
+$(lua-dostring $(bob))
+$(lua-dostring bob(5))
+
+$(info $(lua-pcall proc))
+
 .PHONY: hello
-
-$(info $(lua a = 5 + 1;))
-
 hello:
