ad27b168ddf243afdbab58011fa1e223b86967b6
Stefan Janhunen <sjanhunen@gmail.com>
2018-03-18 21:58:04 -0400

    Sketch out initial lua-pcall & lua-dostring

--- prototype/luamake/luamake.c
+++ prototype/luamake/luamake.c
@@ -7,9 +7,25 @@
 int plugin_is_GPL_compatible;
 static lua_State *ls;
 
-char *gm_luamake(const char *nm, unsigned int argc, char **argv)
+char *gm_lua_dostring(const char *nm, unsigned int argc, char **argv)
 {
+    // TODO: add proper error handling
     int status = luaL_dostring(ls, argv[0]);
+    if(status) {
+        const char *msg = lua_tostring(ls, -1);
+        char *buf = gmk_alloc(strlen(msg) + 1);
+        strcpy(buf, msg);
+        return buf;
+    }
+
+    return 0;
+}
+
+char *gm_lua_pcall(const char *nm, unsigned int argc, char **argv)
+{
+    // TODO: add proper error handling
+    lua_getglobal(ls, argv[0]);
+    int status = lua_pcall(ls, 0, 0, 0);
     if(status) {
         const char *msg = lua_tostring(ls, -1);
         char *buf = gmk_alloc(strlen(msg) + 1);
@@ -23,7 +39,9 @@ char *gm_luamake(const char *nm, unsigned int argc, char **argv)
 int luamake_gmk_setup ()
 {
     ls = luaL_newstate();
-    gmk_add_function ("lua", gm_luamake, 1, 1, 0);
+    luaL_openlibs(ls);
+    gmk_add_function ("lua-dostring", gm_lua_dostring, 1, 1, 0);
+    gmk_add_function ("lua-pcall", gm_lua_pcall, 1, 1, 0);
     return 1;
 }
 
