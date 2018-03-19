5ec2e2668eb2cacac3d7bdc22050d784ba6ea3f9
Stefan Janhunen <sjanhunen@gmail.com>
2018-03-18 20:28:07 -0400

    Integrate lua into loadable gnumake object

--- prototype/luamake/luamake.c
+++ prototype/luamake/luamake.c
@@ -1,22 +1,30 @@
+#include <string.h>
+#include <gnumake.h>
 #include <lua.h>
 #include <lauxlib.h>
 #include <lualib.h>
 
-int main(int argc, char *argv[])
+int plugin_is_GPL_compatible;
+static lua_State *ls;
+
+char *gm_luamake(const char *nm, unsigned int argc, char **argv)
 {
-    lua_State *ls;
-    int status;
-
-    ls = luaL_newstate();
-
-    if(argc > 1) {
-        status = luaL_dostring(ls, argv[1]);
-        if(status) {
-            printf("ERROR: %s\n", lua_tostring(ls, -1));
-        }
+    int status = luaL_dostring(ls, argv[0]);
+    if(status) {
+        const char *msg = lua_tostring(ls, -1);
+        char *buf = gmk_alloc(strlen(msg) + 1);
+        strcpy(buf, msg);
+        return buf;
     }
-
-    lua_close(ls);
 
     return 0;
 }
+
+int luamake_gmk_setup ()
+{
+    ls = luaL_newstate();
+    gmk_add_function ("lua", gm_luamake, 1, 1, 0);
+    return 1;
+}
+
+// TODO: where do we lua_close(ls)
