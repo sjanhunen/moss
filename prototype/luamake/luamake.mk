53b8f6e9d873d53595bc2234158ebe6b4a725c69
Stefan Janhunen <sjanhunen@gmail.com>
2018-03-19 23:21:22 -0400

    Experiment with more make/lua integration

--- prototype/luamake/luamake.mk
+++ prototype/luamake/luamake.mk
@@ -1,22 +1,21 @@
 load luamake.so
 
-define bob
+lua-loadfile = $(lua loadfile("$1")())
 
-bob = function(a)
-	return a;
-end
+# Load lua code
+$(call lua-loadfile,luamake.lua)
 
-proc = function(msg1, msg2)
-	local welcome = "Hello from Lua: ";
-	return welcome .. msg1 .. msg2;
-end
+# Clean direct evaluation of Lua code
+$(lua print("Print from Lua!"))
+$(info Result is $(lua return 5 + 5))
 
-endef
+# Call function through load
+# (arguments are evaluated as part of code)
+$(info $(lua return proc('hi', 'another hello')))
 
-$(info $(lua-dostring $(bob)))
-$(info $(lua-dostring bob(5)))
-
-$(info $(lua-pcall proc, hi, another hello))
+# Call function directly
+# (every argument explicitly a string)
+$(info $(lua-pcall proc,hi,another hello))
 
 .PHONY: hello
 hello:
