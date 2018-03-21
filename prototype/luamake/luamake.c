5e8d0a110d079757c861c433e48e38693b4f443a
Stefan Janhunen <sjanhunen@gmail.com>
2018-03-20 22:28:16 -0400

    Improve error handling in luamake.c

--- prototype/luamake/luamake.c
+++ prototype/luamake/luamake.c
@@ -10,21 +10,38 @@ static lua_State *ls;
 char *gm_lua_pcall(const char *nm, unsigned int argc, char **argv)
 {
     if(argc >= 1) {
-        // TODO: add proper error handling
         lua_getglobal(ls, argv[0]);
         for(int i = 1; i < argc; ++i) {
             lua_pushstring(ls, argv[i]);
         }
         int status = lua_pcall(ls, argc - 1, 1, 0);
         const char *msg = lua_tostring(ls, -1);
-        if(msg != 0) {
-            char *buf = gmk_alloc(strlen(msg) + 1);
-            strcpy(buf, msg);
-            return buf;
+        if(status) {
+            const char *format =
+                "define _lua_error_\n"
+                "%s\n"
+                "endef\n"
+                "$(error $(_lua_error_))\n";
+
+            // Formatted string is guaranteed to be shorter than format + msg
+            size_t len = strlen(msg) + strlen(format);
+            char *err_msg = gmk_alloc(len);
+            if(err_msg) {
+                snprintf(err_msg, len, format, msg);
+                gmk_eval(err_msg, NULL);
+            }
+        }
+        else if(msg != NULL) {
+            size_t len = strlen(msg) + 1;
+            char *result = gmk_alloc(strlen(msg) + 1);
+            if(result) {
+                strncpy(result, msg, len);
+                return result;
+            }
         }
     }
 
-    return 0;
+    return NULL;
 }
 
 int luamake_gmk_setup ()
