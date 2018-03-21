9d0a369d095c4e8e7e4143eaebc50482d2102794
Stefan Janhunen <sjanhunen@gmail.com>
2018-03-20 21:51:41 -0400

    Streamline gnumake/lua integration to 1 function

--- prototype/luamake/luamake.mk
+++ prototype/luamake/luamake.mk
@@ -1,26 +1,16 @@
 load luamake.so
 
-lua-loadfile = $(lua loadfile("$1")())
-
 # Load lua code from external file
-$(lua-pcall dofile,luamake.lua)
+$(lua dofile,luamake.lua)
 
 # Clean direct evaluation of Lua code
-$(lua-pcall print,This is a print from Lua)
-
-# Evaluate string as code
-$(info Result is $(lua return (5 + 5) / 3))
+$(lua print,This is a print from Lua)
 
 # Call custom eval function to evaluate string as code
-$(info Result is $(lua-pcall eval, (5+5) / 3))
-
-# Call function through load
-# (arguments are evaluated as part of code)
-$(info $(lua return proc('hi', 'another hello')))
+$(info The result is $(lua eval, (5+5) / 3))
 
 # Call function directly
-# (every argument explicitly a string)
-$(info $(lua-pcall proc,hi,another hello))
+$(info $(lua proc,hi,another hello))
 
 .PHONY: hello
 hello:
