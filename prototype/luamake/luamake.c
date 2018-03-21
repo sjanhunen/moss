6be6603d2dfd4b743a040c053b1902d24bf0fb60
Stefan Janhunen <sjanhunen@gmail.com>
2018-03-20 21:51:41 -0400

    Streamline gnumake/lua integration to 1 function

--- prototype/luamake/luamake.c
+++ prototype/luamake/luamake.c
@@ -7,21 +7,6 @@
 int plugin_is_GPL_compatible;
 static lua_State *ls;
 
-char *gm_lua_dostring(const char *nm, unsigned int argc, char **argv)
-{
-    // TODO: add proper error handling
-    int status = luaL_dostring(ls, argv[0]);
-    const char *msg = lua_tostring(ls, -1);
-    // TODO: figure out how best to detect and handle return values
-    if(msg) {
-        char *buf = gmk_alloc(strlen(msg) + 1);
-        strcpy(buf, msg);
-        return buf;
-    }
-
-    return 0;
-}
-
 char *gm_lua_pcall(const char *nm, unsigned int argc, char **argv)
 {
     if(argc >= 1) {
@@ -46,9 +31,9 @@ int luamake_gmk_setup ()
 {
     ls = luaL_newstate();
     luaL_openlibs(ls);
-    gmk_add_function ("lua-dostring", gm_lua_dostring, 1, 1, 0);
-    gmk_add_function ("lua-pcall", gm_lua_pcall, 1, 32, 0);
-    gmk_add_function ("lua", gm_lua_dostring, 1, 1, 0);
+
+    gmk_add_function ("lua", gm_lua_pcall, 1, 32, 0);
+
     return 1;
 }
 
