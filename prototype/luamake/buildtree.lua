30114991ae0f0896ea9975c8c8c2f82297f2ddc1
Stefan Janhunen <sjanhunen@gmail.com>
2018-05-06 09:06:35 -0400

    Evaluate streamlined using(...) for tools and config

--- prototype/luamake/buildtree.lua
+++ prototype/luamake/buildtree.lua
@@ -10,16 +10,11 @@ end
 -- Directive
 function build(artifact)
     if(type(artifact) == "string") then
-        print("build directory: " .. artifact);
+        print("build sub-directory: " .. artifact);
     else
-        print("build artifact: " .. artifact.name);
+        print("build in place");
     end
     return function(e) return artifact end
-end
-
--- Directive
-function with(g)
-    return function(c) return c end
 end
 
 -- Directive
@@ -38,53 +33,43 @@ main_image = artifact("executable") {
     libs = {math_lib};
 };
 
--- The build directive is one of the most important concepts within moss
--- build(<artifact>) { }
--- A build directive may contain
---  - Nested 'build' directive (for directories only)
---  - 'with' directives for specializing genes
---  - 'using' directives for selecting tools
---  This encourages a natural language syntax like build(artifact) with(gene) using(tool)
+-- The build tree is one of the most important concepts within moss.
+-- Its function is to select and specialze genes for artifacts within the node.
 
--- Build structures used to explicitly define everything that is created
--- A directory artifact without name could imply current directory
 build("output") {
-    using(clangld, clangcc);
+    using(clangcc) { cflags = "-Wall" };
+    using(clangld);
 
-    with(myconfig) {
+    using(myconfig) {
         memory_model = "large";
         debug = false;
     };
 
     build("debug") {
+        main_image,
         -- math_lib will be created in this build as a dependency of main_image
-        build(main_image);
-
-        with(clang_config) {
-            -- math_lib will inherit these cflags
-            cc = { cflags = "-Og -DDEBUG" };
-        };
+        using(clangcc) { cflags = "-Og -DDEBUG" };
     };
+
     build("release") {
-        -- specify math_lib explicitly for this build
-        build(math_lib) {
-            -- special options for this build of math_lib
-            with(clang_config) { flags = "-Mfpu" };
-        };
+        main_image,
+        using(clangcc) { cflags = "-O3" };
+        using(myconfig) { debug = true };
 
-        build(main_image);
-
-        with(clang_config) {
-            cc = { cflags = "-O3" };
+        -- Explicit options for this build of math_lib
+        build {
+            math_lib,
+            using(clangcc) { cflags = "-Mfpu" };
         };
-        with(myconfig) { debug = true };
     };
 
-    build("package") {
-        using(gzip);
-        build(artifact("zipfile") {
+    -- In-place build and artifact definition
+    build {
+        artifact("zipfile") {
             name = "release.zip";
+            -- TODO: fix ambiguous main image reference
             source = { main_image, "help.doc", "release-notes.txt" };
-        });
-    }
+        },
+        using(gzip) { flags = "--best" };
+    };
 };
