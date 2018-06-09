411b88051409d4c7aa91a7ae76a03ed3e039f322
Stefan Janhunen <sjanhunen@gmail.com>
2018-06-09 13:04:27 -0400

    Prototype functional build pipeline concept

--- prototype/luamake/build.lua
+++ prototype/luamake/build.lua
@@ -1,27 +1,58 @@
 -- Builds:
---  * Every build artifact is defined by an associated build
+--  * Each build artifact is defined by an associated build table
+--  * Everything required to build the artifact (variables and rules) are specified in the table
+--  * The build function returns a build pipeline that transforms a build table
+--  * The output of a build pipeline is a build table
+--  * Rules and variables are composed and modified through the pipeline
 --  * Builds contain one or more local build variables
 --  * Build variables can be defined directly
---  * Build variables can be composed from traits
+--  * Build variables can be composed from the pipeline
 --  * Build variables can expanded within strings
---  * Build variables can be expanded directly
---  4) Builds are immutable and can be cloned + extended as easily as
---      new_build = existing_build { <definitions> };
---
---  -- A list of traits could be provided to each build
---  b = build(<tool>, <traits>...);
---
---  -- Then, traits would need to be configured
---  b {
---      [trait1] = { ... };
---      [trait2] = { ... };
---  }
 
-function build(form)
-    if(type(form) == "string") then
-        print("build directory: " .. form);
-    else
-        print("build form: " .. form.name);
+function extend(variable, value)
+    return function(bt)
+        bt[variable] = bt[variable] .. " " .. value;
+        return bt;
     end
-    return function(e) return function() return form end end
+end
+
+function prefix(value)
+    return function(bt)
+        bt.name = value ..bt.name;
+        return bt
+    end
+end
+
+function clone(bt)
+    local copy = {}
+    for k, v in pairs(bt) do
+        copy[k] = v
+    end
+    return copy
+end
+
+function build(...)
+    local pipeline = {...}
+    return function(bt) 
+        bt = clone(bt)
+        -- TODO: Run recursively on nested builds
+        -- and perform a deep copy
+        for i,step in ipairs(pipeline) do
+            -- TODO: remove compatibility shim with example refactor
+            if type(step) == "function" then
+                bt = step(bt);
+            end
+        end
+        return bt;
+    end
+end
+
+function dumpbuild(bt)
+    for k, v in pairs(bt) do
+        if type(k) == "string" then
+            print(k .. ":" .. v)
+        elseif type(k) == "number" then
+            dumpbuild(v)
+        end
+    end
 end
