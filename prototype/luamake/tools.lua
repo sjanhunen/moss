559c42513c61f4320ee4d71bf8d4f16362440f27
Stefan Janhunen <sjanhunen@gmail.com>
2018-07-07 12:55:26 -0400

    Update tool prototype with latest rule concepts

--- prototype/luamake/tools.lua
+++ prototype/luamake/tools.lua
@@ -1,21 +1,23 @@
-function translate(src, dst)
-    if(dst == nil) then
-        return function(recipe)
-            print("translate " .. src .. " -> OBJ using recipe " .. recipe);
-            return recipe;
-        end
-    else
-        return function(recipe)
-            print("translate " .. src .. " -> " .. dst .. " using recipe " .. recipe);
-            return recipe;
-        end
-    end
+-- Would a generate(pattern, fn) tool be helpful for generated source code?
+
+function translate(src, dst, recipe)
+	return function(bt)
+		-- TODO: return the rule in addition to the recipe
+		return recipe(bt);
+	end
 end
 
-function form(artifact)
-    return function(recipe)
-        print("form " .. artifact .. " using recipe " .. recipe);
-        return recipe;
+function compile(src, recipe)
+	return function(bt)
+		-- TODO: return the rule in addition to the recipe
+		return recipe(bt);
+	end
+end
+
+function form(recipe)
+    return function(bt)
+		-- TODO: return the rule in addition to the recipe
+        return recipe(bt);
     end
 end
 
@@ -23,21 +25,34 @@ xml2cpp_tool = "/opt/tool/xml2cpp";
 clang_home = "/opt/bin/clang";
 
 -- Expansion within recipe templates:
--- ${<variable>} - expands to value of named <variable>
--- $@, $<, $^ - passed through to gnumake
+-- ${<variable>} - expands to value of named variable in current scope or in build table
+-- $@, $<, $^, $(...) - passed through to gnumake
+function expand(bt, recipe)
+	-- TODO: implement
+	return recipe
+end
 
--- Translate from one source file to another source file
-xml2cpp = translate(".xml", ".cpp") "${xml2cpp_tool} -o $@ $^";
+xml2cpp = translate(".xml", ".cpp", function(bt)
+	return expand(bt, "${xml2cpp_tool} -o $@ $^")
+end)
 
--- Translate from source file to object file (i.e. compile)
-clangcc = translate(".cpp")
-    "${clang_home}/clangcc           \z
-        $(addprefix -D,${defines})   \z
-        $(addprefix -D,${defines})   \z
-        -o $@ $^";
+clangcc = compile(".cpp", function(bt)
+    return expand(bt,
+		"${clang_home}/clangcc   		\z
+        $(addprefix -D,${defines})   	\z
+        $(addprefix -D,${defines})   	\z
+        -o $@ $^")
+end)
 
--- Would a generate(pattern, fn) tool be helpful for generated code?
+clangld = form(function(bt)
+	return expand(bt, "${clang_home}/clangld -o $@ $<")
+end)
 
-clangld = form("executable") "${clang_home}/clangld -o $@ $<";
+clangar = form(function(bt)
+	return expand(bt, "${clang_home}/clangar -o $@ $<")
+end)
 
-clangar = form("staticlib") "${clang_home}/clangar -o $@ $<";
+print(xml2cpp())
+print(clangcc())
+print(clangld())
+print(clangar())
