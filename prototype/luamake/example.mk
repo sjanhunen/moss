212990107cc97ea130feaaeb111d80d30b5471b0
Stefan Janhunen <sjanhunen@gmail.com>
2018-08-11 10:51:47 -0400

    Cleanup example to use only build functions

--- prototype/luamake/example.mk
+++ prototype/luamake/example.mk
@@ -3,14 +3,10 @@ hello:
 
 include luamake.mk
 
-# Load lua code from external file
+# Load lua modules
 $(lua require,luamake)
-
-# Call built-in function directly
-$(lua print,This is a print from Lua)
-
-# Call custom eval function to evaluate string as code
-$(info The result is $(lua eval, (5+5) / 3))
+$(lua require,build)
+$(lua require,example)
 
 # Call module function directly
 $(info $(lua proc,hi,another hello))
