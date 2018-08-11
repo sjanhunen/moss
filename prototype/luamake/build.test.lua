b24673003cc29b91f1ea4279b8773fe0b11778f7
Stefan Janhunen <sjanhunen@gmail.com>
2018-08-11 10:38:52 -0400

    Add unit test for composition of build functions

--- prototype/luamake/build.test.lua
+++ prototype/luamake/build.test.lua
@@ -1,14 +1,22 @@
-build = require("build")
+require("build")
 
 describe("Moss build pipeline", function()
 	describe("extend", function()
-		it("should create non-existant variables", function()
+		it("creates non-existant variables", function()
 			local result = extend("FLAG", "1") {}
 			assert.are.same({FLAG=" 1"}, result)
 		end)
-		it("should append to existing variables", function()
+		it("appends to existing variables", function()
 			local result = extend("FLAG", "2") { FLAG = "1" }
 			assert.are.same({FLAG="1 2"}, result)
 		end)
 	end)
+    describe("build", function()
+        it("composes two functions", function()
+            local f1 = extend("F", "1")
+            local f2 = extend("F", "2")
+            local result = build(f1, f2) { F = "0" }
+            assert.are.same({F = "0 1 2"}, result)
+        end)
+    end)
 end)
