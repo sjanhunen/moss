d2dd0d9fca30fb79e12215f2a99d5d09f18fd79e
Stefan Janhunen <sjanhunen@gmail.com>
2018-08-11 16:03:03 -0400

    Implement deep clone for build table

--- prototype/luamake/build.test.lua
+++ prototype/luamake/build.test.lua
@@ -18,5 +18,24 @@ describe("Moss build pipeline", function()
             local result = build(f1, f2) { F = "0" }
             assert.are.same({F = "0 1 2"}, result)
         end)
+        it("deep clones build table", function()
+            local expected = {
+                { f = "a" };
+                { f = "b" };
+            }
+
+            local nested1 = { f = "a" }
+            local nested2 = { f = "b" }
+
+            local actual = build()({
+                nested1;
+                nested2;
+            })
+            assert.are.same(expected, actual)
+
+            nested1.f = "e"
+            nested2.f = "f"
+            assert.are.same(expected, actual)
+        end)
     end)
 end)
