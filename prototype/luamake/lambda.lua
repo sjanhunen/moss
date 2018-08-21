ba0987fef4bd2dbfafac5eec9f2705c94adc7fea
Stefan Janhunen <sjanhunen@gmail.com>
2018-08-21 11:33:31 -0400

    Implement initial lambda concept with test case

--- prototype/luamake/lambda.lua
+++ prototype/luamake/lambda.lua
@@ -1,7 +1,21 @@
+function lambda(op)
+    return function(bt)
+        -- TODO: Apply recursively for nested builds
+        -- TODO: clone table items before applying
+        for name,step in pairs(op) do
+            bt[name] = step(bt[name])
+        end
+        return bt;
+    end
+end
+
 function append(item)
     return function(list)
         if list == nil then
             list = {}
+        end
+        if type(list) ~= "table" then
+            list = { list }
         end
         table.insert(list, item)
         return list
