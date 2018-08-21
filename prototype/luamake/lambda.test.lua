1a8385ef606c6c66bb0afd97170568ee1a9e3c37
Stefan Janhunen <sjanhunen@gmail.com>
2018-08-21 11:33:31 -0400

    Implement initial lambda concept with test case

--- prototype/luamake/lambda.test.lua
+++ prototype/luamake/lambda.test.lua
@@ -1,4 +1,18 @@
 require("lambda")
+require("build")
+
+describe("lambda", function()
+    local set = function(value)
+        return function(entry)
+            return value
+        end
+    end
+    it("applies single operation", function()
+        local fn = lambda { name = set("after") }
+        local output = fn { name = "before" }
+        assert.are.same("after", output.name)
+    end)
+end)
 
 describe("append", function()
     it("creates list from nil list", function()
@@ -15,6 +29,11 @@ describe("append", function()
         local item1 = "BOB"
         local item2 = "ALICE"
         local list = append(item2)({item1})
+        assert.are.same({item1, item2}, list)
+    end)
+    it("appends to non-list", function()
+        local item1 = "BOB"
+        local list = append(item2)(item1)
         assert.are.same({item1, item2}, list)
     end)
 end)
