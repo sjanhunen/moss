a79f13a82d91ecd4a335851ad4f5442c3082fb2d
Stefan Janhunen <sjanhunen@gmail.com>
2018-10-06 15:09:06 -0400

    Capture new build tree concepts

--- prototype/luamake/example.lua
+++ prototype/luamake/example.lua
@@ -29,12 +29,44 @@ local debug_build = build(clang.debug, debug, verbose)
 -- Release build pipeline
 local release_build = build(clang.release, fast)
 
-math_lib = build(clang.staticlib) {
+
+-- Refactor to core concepts of the build tree:
+--
+-- Artifacts are leaf nodes. An artifact is never a parent. An artifact is not a build.
+-- Builds are parents nodes. A build always has one or more children. A build may contain nested builds. A build is not an artifact.
+-- The Root build as the top level build containing all nested builds.
+-- Operators are functions that operate recursively on all descendant artifacts within a node.
+--
+-- artifact(op1, op2, ... opn) { k1 = v1; k2 = v2; k3 = v3 }
+--
+-- build(op1, op2, ... opn) { n1 = artifact1, n2 = artifact2, n3 = artifact3, n4 = build(...) { ... } }
+--
+-- Names of artifacts and nested builds are given within the build definition itself.
+-- This enables clear referencing of nodes through the root or through relative paths.
+--
+-- build(op1, op2, .. opN) {
+--  name1 = artifact(...) {};
+--  name2 = artifact(...) {};
+--  name3 = build(...) { };
+-- }
+--
+-- Builds may also be defined as a sequence of ordered steps (without named artifacts):
+--
+-- build(...) {
+--	step1, step2, step3
+-- }
+--
+-- Are artifacts build before or after steps if they are combined?
+
+
+local artifact = build
+
+math_lib = artifact(clang.staticlib) {
     name = "fastmath.lib";
     source = {"math1.c", "math2.c"};
 };
 
-main_image = build(exe.rule) {
+main_image = artifact(exe.rule) {
     name = "main.exe";
     source = "main.c";
     -- main_image requires math_lib within its build
@@ -56,7 +88,7 @@ spore.exports = build(subdir("output"), clangexe) {
     };
 
     -- In-place build artifact
-    build(zipfile) {
+    artifact(zipfile) {
         name = "release.zip";
 
         files = {
