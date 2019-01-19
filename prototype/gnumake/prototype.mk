75e01407892c47688730bded5f6a29d54b8de286
Stefan Janhunen <sjanhunen@gmail.com>
2019-01-19 16:44:10 -0500

    Add make concept for artifact/build/gene

--- prototype/gnumake/prototype.mk
+++ prototype/gnumake/prototype.mk
@@ -32,5 +32,54 @@ armv5/two.depends = armv4/base
 armv5/two.c.defines = USE_FPU
 armv5/two.code = $(two.code) src/arm/*
 
+# TODO: consider how core concepts might map back into
+# a pure gnumake-based implementation of moss
+#
+# - Each build is described within a single Makefile
+# - Nested builds are created with recursive make (a very targeted recursion)
+# - Namespace is maintained with 'structures' and sub-processes
+# - Structures can be created with some of the prototype approaches here
+
+# Compare to luamake example
+# Makefile
+TOOLS = clangexe clangcc clangc++
+
+# Explicit approach
+myzip.files = release/main.exe debug/main.exe help.doc release-notes.txt
+myzip.name = release.zip
+myzip.rules = zipfile
+
+# Recursive builds to traverse
+BUILDS = debug/Makefile release/Makefile
+
+ARTIFACTS = myzip
+
+############################
+# debug/Makefile
+############################
+
+-include $(HOME)/lib/math.mk
+-include $(HOME)/app/main.mk
+
+GENES = debug_build
+
+ARTIFACTS = math_lib main_image
+
+############################
+# release/Makefile
+############################
+
+-include $(HOME)/lib/math.mk
+-include $(HOME)/app/main.mk
+
+GENES = release_build
+
+ARTIFACTS = math_lib main_image
+
+math_lib.genes += clang.fpu
+math_lib.genes += clang.o3
+
+# Back to prototype
+
 include spore.mk
 include doc.mk
