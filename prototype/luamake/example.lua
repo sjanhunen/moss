e4f6780e5b521204a64efad25fd3d9009b9e6579
Stefan Janhunen <sjanhunen@gmail.com>
2019-02-02 18:49:34 -0500

    Refine core moss concept definitions

--- prototype/luamake/example.lua
+++ prototype/luamake/example.lua
@@ -1,12 +1,8 @@
 -- Fundamental concepts:
---  Gene: operator pairs used to expand artifact definitions
---  Artifact: the product of gene sequence applied to base definition
---  Build: named collections of artifacts and nested builds
---
--- Derived concepts:
---  Tool:
---  Rule:
---  Trait:
+--  Trait: fundamental building block of configuration and reuse
+--  Artifact: an output product present in at least one build
+--  Rule: patterns and recipes for transpiling, compiling, and forming artifacts
+--  Build: named collections of artifacts, potentially containing other nested builds
 
 require("gene")
 require("artifact")
@@ -24,12 +20,6 @@ local debug = gene { cflags = append "-DDEBUG" }
 local fast = gene { cflags = append "-DLOG_NONE" }
 local verbose = gene { cflags = append "-DLOG_VERBOSE" }
 
--- Debug build pipeline
-local debug_build = build(clang.debug, debug, verbose)
-
--- Release build pipeline
-local release_build = build(clang.release, fast)
-
 math_lib = artifact(clang.staticlib) {
     name = "fastmath.lib";
     source = {"math1.c", "math2.c"};
@@ -45,14 +35,28 @@ main_image = artifact(exe.rule) {
 -- The spore global would be used to "export" artifacts
 spore = {}
 
+-- This is a placeholder for an actual trait that
+-- configures build options
+build_options = {}
+
 -- TODO: refactor according to new build design thinking
 spore.exports = build(subdir("output"), clangexe) {
-    build(subdir("debug"), debug_build) {
+    [build_options] = {
+        debug = false;
+        log = true;
+        secure = false;
+    };
+
+    build(subdir("debug")) {
+        [build_options] = { debug = true };
+
         math_lib;
         main_image;
     };
 
-    build(subdir("release"), release_build) {
+    build(subdir("release")) {
+        [build_options] = { secure = true };
+
         main_image;
         build(clang.fpu)(math_lib);
     };
