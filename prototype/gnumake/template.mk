14bb4083d94f450038612d8b7efde051ea6c5489
Stefan Janhunen <sjanhunen@gmail.com>
2019-09-07 12:31:13 -0400

    Capture TEMPLATE concepts

---
+++ prototype/gnumake/template.mk
@@ -0,0 +1,38 @@
+# Templates are used to create the rules and recipes required for
+# artifacts and their associated dependencies.
+#
+# A template has the following table members:
+# 	RECIPE - the command(s) used to create the artifact or intermediate
+# 	PREREQ - any dependencies required by the final artifact
+# 	TARGET - for intermediates, the implicit target pattern
+#
+# Note that targets are specified explicitly for artifacts.  
+
+# Example for CPP compilation
+cpp.RECIPE = cc -c -o $$@ $$^
+cpp.TARGET = %.obj.$1
+cpp.PREREQ = %.cpp | $($1.DIRS)
+
+# Example for final executable artifact
+define exe.RECIPE
+ld -o $$@ $$^ $($1.LIBS)
+endef
+exe.PREREQ = $($1.OBJS) $($1.LIBS)
+# Any templates required to create artifact
+exe.TEMPLATES = cpp
+
+# How do we associate artifact template with final artifact?
+# We could pass the TEMPLATE into the ARTIFACT call
+# Should we call ARTIFACT TEMPLATE instead?
+bin/host/myprogram.exe: $(call ARTIFACT, myprogram, host.exe)
+bin/arm/myprogram.alf: $(call ARTIFACT, myprogram, arm.exe)
+bin/arm/myprogram.bin: $(call ARTIFACT, myprogram, arm.bin)
+
+# Or we could insist that each definition already has the TEMPLATE applied?
+# In this case, could we potentially have multiple outputs?
+myprogram-host.exe: $(call ARTIFACT, myprogram_host)
+bin/arm/myprog.elf bin/arm/myprog.bin: $(call ARTIFACT, myprogram_target)
+
+# Note that if we want object directory structure to follow target directory
+# structure, we can't use this form of ARTIFACT macro. We need to use the form
+# $(call ARTIFACT, bin/host/myprogram.exe, myprogram, ...)
