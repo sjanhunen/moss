80676302e47e8885a8c2f54fa084c00b9f368473
Stefan Janhunen <sjanhunen@gmail.com>
2019-09-07 21:15:07 -0400

    Implement prerequisite RULES for TEMPLATE

--- prototype/gnumake/template.mk
+++ prototype/gnumake/template.mk
@@ -1,40 +1,41 @@
 # Templates are used to create the rules and recipes required for
-# artifacts and their associated dependencies.
+# artifacts and their associated prerequisites.
 #
 # A template has the following table members:
 # 	RECIPE - the command(s) used to create the artifact or intermediate
 # 	PREREQ - any dependencies required by the final artifact
 # 	TARGET - for intermediates, the implicit target pattern
+# 	RULES - for artifacts that have prerequisites
 #
-# Note that targets are specified explicitly for artifacts.  
+# Artifacts are defined through creation of a new target.
+# Targets are specified explicitly for artifacts in the rule.
 
-# Example for CPP compilation
-cpp.RECIPE = cc -c -o $$@ $$^
-cpp.TARGET = %.obj.$1
-cpp.PREREQ = %.cpp | $($1.DIRS)
+# Option 1:
+#
+# bin/host/myprogram.exe: $(call TEMPLATE, myprogram, host.exe)
+#
+# bin/arm/myprogram.alf bin/arm/myprogram.bin: $(call TEMPLATE, myprogram, arm.exe)
+#
 
-# Example for final executable artifact
-define exe.RECIPE
-ld -o $$@ $$^ $($1.LIBS)
-endef
-exe.PREREQ = $($1.OBJS) $($1.LIBS)
-# Any templates required to create artifact
-exe.TEMPLATES = cpp
-
-# How do we associate artifact template with final artifact?
-# We could pass the TEMPLATE into the TEMPLATE call
-# Should we call TEMPLATE TEMPLATE instead?
-bin/host/myprogram.exe: $(call TEMPLATE, myprogram, host.exe)
-# Could we potentially have multiple outputs?
-bin/arm/myprogram.alf bin/arm/myprogram.bin: $(call TEMPLATE, myprogram, arm.exe)
-
-# Or should we insist that each definition already has the TEMPLATE applied?
-# Could we potentially have multiple outputs?
-myprogram-host.exe: $(call TEMPLATE, myprogram_host)
-bin/arm/myprog.elf bin/arm/myprog.bin: $(call TEMPLATE, myprogram_target)
+# Option 2:
+#
+# myprogram-host.exe: $(call TEMPLATE, myprogram_host)
+# bin/arm/myprog.elf bin/arm/myprog.bin: $(call TEMPLATE, myprogram_target)
 
 # Note that if we want object directory structure to follow target directory
 # structure, we might have trouble with this form of TEMPLATE. We may need
-$(call TEMPLATE, bin/host/myprogram.exe, myprogram, ...)
-
+#
+# $(call TEMPLATE, bin/host/myprogram.exe, myprogram, ...)
+#
 # Need to carefully examine whether we can automatically match object DIRS to target
+
+define _RULES
+$(call $1.TARGET,$2): $(call $1.PREREQ,$2); $(call $1.RECIPE,$2)
+endef
+
+define _TEMPLATE
+$(call $1.PREREQ,$1); $(call $1.RECIPE,$1)
+$(foreach t,$($1.RULES),$(eval $(call _RULES,$t,$1)))
+endef
+
+TEMPLATE = $(call _TEMPLATE,$(strip $1))
