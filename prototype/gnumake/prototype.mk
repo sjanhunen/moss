1c2cf140ddf3ebb8748b518a89062eb67df0cf2e
Stefan Janhunen <sjanhunen@gmail.com>
2019-09-28 20:54:35 -0400

    Improve prototype examples

--- prototype/gnumake/prototype.mk
+++ prototype/gnumake/prototype.mk
@@ -20,18 +20,37 @@ $1.templates = c.template exe.template
 $1.obj = bin/table.o
 endef
 
+define my_mutation
+$1.my_mutation = 1
+endef
+
 bin: bin/host bin/target
 
+define host_output
+$(table)
+endef
+
+define target_output1
+$(table)
+$(my_mutation)
+$1.name = bin/target/name1.out
+endef
+
+define target_output2
+$(table)
+$1.name = bin/target/name2.out
+endef
+
 # Easily clone tables with default artifact names
-bin/host/name1.out = $(table)
-bin/target/name1.out = $(table)
-bin/target/name2.out = $(table)
+bin/host/name1.out = $(host_output)
 
 bin/host: $(call ARTIFACT, bin/host/name1.out)
 
 # TODO: why does build fail if both artifacts are on same line?
-bin/target: $(call ARTIFACT, bin/target/name1.out)
-bin/target: $(call ARTIFACT, bin/target/name2.out)
+bin/target: $(call ARTIFACT, target_output1)
+bin/target: $(call ARTIFACT, target_output2)
+
+# TODO: consider extending ARTIFACT to accepts pattern like target_%
 
 define special1
 $(table)
