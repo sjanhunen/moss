2877dc50aa1df67d5a5a30f72c63f5b1fd08a1de
Stefan Janhunen <sjanhunen@gmail.com>
2019-11-16 02:17:24 +0000

    Add support for strings (non-table) to luafile

--- prototype/luamake/luafile.lua
+++ prototype/luamake/luafile.lua
@@ -1,13 +1,18 @@
 function dumptable(bt, tablename)
     local tabledef = {}
     local output = {}
+    local prefix = ""
+
+    if tablename then
+        prefix = "$1."
+    end
 
     for k, v in pairs(bt) do
         if type(k) == "string" then
             if type(v) == "table" then
                 if #v > 0 then
                     -- This is an ordered list value
-                    table.insert(tabledef, "$1." .. k .. " = " .. table.concat(v, " "))
+                    table.insert(tabledef, prefix .. k .. " = " .. table.concat(v, " "))
                 else
                     if tablename then
                         -- This is a nested definition
@@ -19,15 +24,19 @@ function dumptable(bt, tablename)
                 end
             else
                 -- This is a string value
-                table.insert(tabledef, "$1." .. k .. " = " .. tostring(v))
+                table.insert(tabledef, prefix .. k .. " = " .. tostring(v))
             end
         end
     end
 
-    if tablename and #tabledef > 0 then
-        table.insert(output, "define " .. tablename)
-        table.insert(output, table.concat(tabledef, "\n"))
-        table.insert(output, "endef")
+    if #tabledef > 0 then
+        if tablename then
+            table.insert(output, "define " .. tablename)
+            table.insert(output, table.concat(tabledef, "\n"))
+            table.insert(output, "endef")
+        else
+            table.insert(output, table.concat(tabledef, "\n"))
+        end
     end
 
     return table.concat(output, "\n")
