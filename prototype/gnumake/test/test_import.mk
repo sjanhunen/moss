07d72cd08c17e9a9d5cad3024cc9950e31d1122f
Stefan Janhunen <sjanhunen@gmail.com>
2019-12-26 15:19:16 -0500

    Add checks for defined variables

--- prototype/gnumake/test/test_import.mk
+++ prototype/gnumake/test/test_import.mk
@@ -6,17 +6,20 @@ ifdef $(call unittest,with_module_prefix)
 
 $(call import,test/module.mk,bob)
 $(call assert_equal, $(bob.variable1), v1)
+$(call assert_equal, $(bob.defn.member1), 4)
 
 endif
 ifdef $(call unittest,with_module_prefix_and_spaces)
 
 $(call import, test/module.mk , sue )
 $(call assert_equal, $(sue.variable1), v1)
+$(call assert_equal, $(sue.defn.member1), 4)
 
 endif
 ifdef $(call unittest,without_module_prefix)
 
 $(call import,test/module.mk)
 $(call assert_equal, $(variable1), v1)
+$(call assert_equal, $(defn.member1), 4)
 
 endif
