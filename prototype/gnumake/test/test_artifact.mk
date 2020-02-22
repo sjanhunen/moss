203fda9eb1c72da0e10083b1aa97b9bbd3a50bd9
Stefan Janhunen <sjanhunen@gmail.com>
2020-02-22 17:49:29 -0500

    Add artifact test with two rules

--- prototype/gnumake/test/test_artifact.mk
+++ prototype/gnumake/test/test_artifact.mk
@@ -2,21 +2,39 @@ include unittest.mk
 include assert.mk
 include artifact.mk
 
-define test_rule
+define single_rule
+.PHONY: $($1.name)
 $($1.name):
 	@echo "$($1.name)!"
 endef
 
-define test_artifact
+define chained_rule_1
+.PHONY: $($1.name)
+$($1.name): $($1.name).dep
+endef
+
+define chained_rule_2
+.PHONY: $($1.name)
+$($1.name).dep:
+	@echo "$($1.name)!"
+endef
+
+define simple_artifact
 $1.name = bob
-$1.rules = test_rule
+$1.rules = single_rule
+endef
+
+define chained_artifact
+$1.name = larry
+$1.rules = chained_rule_1 chained_rule_2
 endef
 
 .PHONY: target
-target: $(call artifact, test_artifact)
+target: $(call artifact, simple_artifact)
+$(call artifact, chained_artifact):
 
 ifdef $(call unittest,with_one_artifact)
-$(call assert_equal,$(test_artifact.name),bob)
+$(call assert_equal,$(simple_artifact.name),bob)
 endif
 
 ifdef $(call unittest,with_one_rule)
@@ -25,4 +43,8 @@ endif
 
 ifdef $(call unittest,with_one_target)
 $(call assert_equal,$(shell make -f $(firstword $(MAKEFILE_LIST)) target),bob!)
+endif
+
+ifdef $(call unittest,with_two_rules)
+$(call assert_equal,$(shell make -f $(firstword $(MAKEFILE_LIST)) larry),larry!)
 endif
