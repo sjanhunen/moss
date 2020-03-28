fab49affe4903c804b6a3c4b90353827b03796b6
Stefan Janhunen <sjanhunen@gmail.com>
2020-03-28 11:28:31 -0400

    Update notes on artifact implementation

--- prototype/gnumake/artifact.mk
+++ prototype/gnumake/artifact.mk
@@ -1,5 +1,13 @@
 ifndef _artifact
 
+# NOTE: it is paramount that rules are expanded last after all other artifact
+# definitions.  We may choose to use decorated template definitions to create
+# rules.  Rules may also share unique namespaces with other templates in a
+# module.  For example, the <artifact>.<rule> namespace could contain settings
+# for rules.
+
+# TODO: add support for default name
+
 define _artifact
 $(eval $(call $1,$1))
 $(foreach rule,$($1.rules),$(eval $(call $(rule),$1)))
